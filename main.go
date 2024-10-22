071ec6654db2e807ef382a9926489bcd53f00f45
guoshijiang <guoshijiang2012@163.com>
2024-10-22 10:40:03 +0800

    fux

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/the-web3/eth-wallet-go/addresss"
 )
 
-func main() {
+func Foo() *int {
+	x := 42
+	return &x // x 逃逸到堆上
+}
 
+func ArraySize() {
+	arr := [3]int{1, 2, 3}
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("元素: %d, 地址: %p\n", arr[i], &arr[i])
+	}
+	fmt.Printf("数组占用的内存大小: %d bytes\n", unsafe.Sizeof(arr))
+}
+
+func main01() {
 	addrInfo, err := addresss.CreateAddressFromPrivateKey()
 	if err != nil {
 		fmt.Println("create address from private fail", "err", err)
@@ -18,4 +31,14 @@ func main() {
 	fmt.Println("address=", addrInfo.PrivateKey)
 	fmt.Println("address=", addrInfo.PublicKey)
 	fmt.Println("address=", addrInfo.Address)
+}
+
+func main02() {
+	ArraySize()
+}
+
+func main03() {
+	s := make([]byte, 200)
+	ptr := unsafe.Pointer(&s[0])
+	fmt.Println(ptr)
 }
